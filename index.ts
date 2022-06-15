e85f349157db7f8a1b035f72e17afcc3b6633ad3
Ale-Costa <alexandrercosta01@gmail.com>
2022-06-15 11:08:25 -0300

    generic types

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-const input = document.getElementById('input') as HTMLInputElement;
+function adicionaApendiceALista<T>(array: any[], valor: T){
+    return array.map(() => valor);
+}
 
-input.addEventListener('input', (event) => {
-    const i = event.currentTarget as HTMLInputElement;
-    console.log(i.value);
-});
+adicionaApendiceALista([1,2,3],1);
